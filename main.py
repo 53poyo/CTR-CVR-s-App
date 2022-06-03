3cfbead636d95ff2232447601b0eccb95db93e65
53poyo <92011038+53poyo@users.noreply.github.com>
2022-06-03 16:30:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -96,6 +96,9 @@ def upload_file():
         return render_template("index.html", answer=answer, answer2=answer2)
     return render_template("index.html", answer="", answer2="")
 
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug = True,host ='0.0.0.0',port = port)
     port = int(os.environ.get('PORT', 8080))
     app.run(debug = True,host ='0.0.0.0',port = port)
 
